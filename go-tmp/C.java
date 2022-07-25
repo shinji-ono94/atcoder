eb69ba573cd8250f8fdeb7c1856eb1f837c0da62
shinji-ono94 <1530sinnzi1003@gmail.com>
2022-07-26 00:03:47 +0900

    最強のテンプレートを作る

---
+++ go-tmp/C.java
@@ -0,0 +1,120 @@
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.PrintWriter;
+import java.util.Arrays;
+import java.util.NoSuchElementException;
+
+public class C implements Runnable {
+    public static void main(String[] args) {
+        new Thread(null, new Main(), "", Runtime.getRuntime().maxMemory()).start();
+    }
+
+    int o(char c) {
+        return c - 'a';
+    }
+
+    public void run() {
+        FastScanner sc = new FastScanner();
+        PrintWriter pw = new PrintWriter(System.out);
+
+        int s1 = sc.nextInt();
+        int ans = 0;
+        for (int i = 0; i < 3; i++) {
+            ans += s1 % 10;
+            s1 /= 10;
+        }
+        pw.println(ans);
+        pw.close();
+    }
+
+    void tr(Object... objects) {
+        System.err.println(Arrays.deepToString(objects));
+    }
+}
+
+class FastScanner {
+    private final InputStream in = System.in;
+    private final byte[] buffer = new byte[1024];
+    private int ptr = 0;
+    private int buflen = 0;
+
+    private boolean hasNextByte() {
+        if (ptr < buflen) {
+            return true;
+        } else {
+            ptr = 0;
+            try {
+                buflen = in.read(buffer);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            if (buflen <= 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    private int readByte() {
+        if (hasNextByte())
+            return buffer[ptr++];
+        else
+            return -1;
+    }
+
+    private static boolean isPrintableChar(int c) {
+        return 33 <= c && c <= 126;
+    }
+
+    private void skipUnprintable() {
+        while (hasNextByte() && !isPrintableChar(buffer[ptr]))
+            ptr++;
+    }
+
+    public boolean hasNext() {
+        skipUnprintable();
+        return hasNextByte();
+    }
+
+    public String next() {
+        if (!hasNext())
+            throw new NoSuchElementException();
+        StringBuilder sb = new StringBuilder();
+        int b = readByte();
+        while (isPrintableChar(b)) {
+            sb.appendCodePoint(b);
+            b = readByte();
+        }
+        return sb.toString();
+    }
+
+    public long nextLong() {
+        if (!hasNext())
+            throw new NoSuchElementException();
+        long n = 0;
+        boolean minus = false;
+        int b = readByte();
+        if (b == '-') {
+            minus = true;
+            b = readByte();
+        }
+        if (b < '0' || '9' < b) {
+            throw new NumberFormatException();
+        }
+        while (true) {
+            if ('0' <= b && b <= '9') {
+                n *= 10;
+                n += b - '0';
+            } else if (b == -1 || !isPrintableChar(b)) {
+                return minus ? -n : n;
+            } else {
+                throw new NumberFormatException();
+            }
+            b = readByte();
+        }
+    }
+
+    public int nextInt() {
+        return (int) nextLong();
+    }
+}
